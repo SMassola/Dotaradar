96f6fe0798c3ed82a9c10dbe0c562eed6aec99d3
Steven Massola <Steven.Massola@gmail.com>
2016-09-24 02:22:28 -0700

    secrets.yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  
+
   config.secret_key_base = ENV["SECRET_KEY_BASE"]
   # Settings specified here will take precedence over those in config/application.rb.
 
--- config/initializers/secret_token.rb
+++
@@ -1,1 +0,0 @@
-Dotaradar::Application.config.secret_key_base = ENV['SECRET_TOKEN']
